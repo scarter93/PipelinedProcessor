ffd10287867892c617ec9d7133ef8e55433ad569
augustelalande <auguste.lalande@gmail.com>
2016-03-16 12:58:06 -0400

    initial architecture for decode and writeback

--- Decode.vhd
+++ Decode.vhd
@@ -6,13 +6,15 @@ entity INSTRUCTION_DECODE is
 
 generic ( DATA_WIDTH : integer := 32
 	);
-port( 	IR_in	: in unsigned(DATA_WIDTH-1 downto 0);
+port( 	clk	: in std_logic;
+	IR_in	: in unsigned(DATA_WIDTH-1 downto 0);
 	PC_in	: in unsigned(DATA_WIDTH-1 downto 0);
 	MEM	: in unsigned(DATA_WIDTH-1 downto 0);	-- location to write back
 	WB_IR	: in unsigned(DATA_WIDTH-1 downto 0);	-- data to write back
 	IR_out	: out unsigned(DATA_WIDTH-1 downto 0);
 	PC_out	: out unsigned(DATA_WIDTH-1 downto 0);
 	IMM	: out unsigned(DATA_WIDTH-1 downto 0);	-- immiediate operand
+	op0	: out unsigned(4 downto 0);
 	op1	: out unsigned(DATA_WIDTH-1 downto 0);
 	op2	: out unsigned(DATA_WIDTH-1 downto 0)
 	);
@@ -20,5 +22,30 @@ port( 	IR_in	: in unsigned(DATA_WIDTH-1 downto 0);
 end entity;
 
 architecture disc of INSTRUCTION_DECODE is
+
+type REGISTERS is array (0 to 31) of unsigned(DATA_WIDTH-1 downto 0);
+signal reg :  REGISTERS;
+
 begin
+
+process clk
+begin
+	if rising_edge(clk) then
+		op0 <= IR_in(31 downto 26);
+		op1 <= reg(IR_in(25 downto 21));
+		op2 <= reg(IR_in(20 downto 16));
+		IMM <= "0000000000000000" & IR_in(15 downto 0);
+		reg(WB_IR(15 downto 11)) <= MEM;
+		reg(0) <= (others => '0'); --ensure $R0 is always 0
+	end if;
+end process;
+
+process clk
+begin
+	if rising_edge(clk) then
+		IR_out <= IR_in;
+		PC_out <= PC_in;
+	end if;
+end process;
+
 end disc;
--- WriteBack.vhd
+++ WriteBack.vhd
@@ -8,6 +8,7 @@ generic ( DATA_WIDTH : integer := 32
 	);
 port(	memory	: in unsigned(DATA_WIDTH-1 downto 0);
 	alu_result	: in unsigned(DATA_WIDTH-1 downto 0);
+    mem_to_reg  : in std_logic;
 	IR_in	: in unsigned(DATA_WIDTH-1 downto 0);
 	IR_out	: out unsigned(DATA_WIDTH-1 downto 0);
 	WB	: out unsigned(DATA_WIDTH-1 downto 0)
@@ -17,4 +18,16 @@ end entity;
 
 architecture disc of WRITE_BACK is
 begin
+
+process clk
+begin
+    if rising_edge(clk) then
+        IR_out <= IR_in;
+        case mem_to_reg is
+            when '0' => WB <= alu_result;
+            when '1' => WB <= memory;
+        end case;
+    end if;
+end process
+
 end disc;
