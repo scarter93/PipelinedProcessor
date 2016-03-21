04431adeb35b269add414d756bcbbb82783e2340
augustelalande <auguste.lalande@gmail.com>
2016-03-21 15:54:15 -0400

    correct-ish sign extension

--- Decode.vhd
+++ Decode.vhd
@@ -30,13 +30,21 @@ begin
 
 process (clk)
 begin
+
+-- jump and link
+-- $R31
+
 	if rising_edge(clk) then
         addr1 <= IR_in(25 downto 21);
 		addr2 <= IR_in(20 downto 16);
         addr3 <= WB_IR(15 downto 11);
         op1 <= reg(to_integer(addr1));
 		op2 <= reg(to_integer(addr2));
-		IMM <= "1111111111111111" & IR_in(15 downto 0);
+		if IR_in(15) = '1' then
+			IMM <= "1111111111111111" & IR_in(15 downto 0);
+		else
+			IMM <= "0000000000000000" & IR_in(15 downto 0);
+		end if;
 		reg(to_integer(addr3)) <= MEM;
 		reg(0) <= (others => '0'); --ensure $R0 is always 0
 	end if;
