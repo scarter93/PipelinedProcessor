11f3243e909b9b091584a15525055979f7d31529
augustelalande <auguste.lalande@gmail.com>
2016-03-27 11:09:23 -0400

    initial work on hazard detection

--- Fetch.vhd
+++ Fetch.vhd
@@ -10,7 +10,7 @@ port(	clk	: in std_logic;
 	reset	: in std_logic;
 	branch_taken	: in std_logic;
 	branch_pc	: in unsigned(DATA_WIDTH-1 downto 0);
-	IR	: out unsigned(DATA_WIDTH-1 downto 0);
+	IR	: out unsigned(DATA_WIDTH-1 downto 0) := to_unsigned(0, DATA_WIDTH);
 	PC_out	: out unsigned(DATA_WIDTH-1 downto 0);
 	-- memory access
 	IR_pc	: out unsigned(DATA_WIDTH-1 downto 0);
@@ -23,14 +23,34 @@ end entity;
 
 architecture disc of INSTRUCTION_FETCH is
 
+type instruction_log is array (1 to 4) of unsigned(DATA_WIDTH-1 downto 0);
+signal IR_log :  instruction_log;
+
 -- signals
 signal PC : unsigned(DATA_WIDTH-1 downto 0) := to_unsigned(0, DATA_WIDTH);
+signal IR_check : unsigned(DATA_WIDTH-1 downto 0) := to_unsigned(0, DATA_WIDTH);
 
 begin
 
-IR <= unsigned(IR_data);
+IR_update : process(clk)
+begin
+	if falling_edge(clk) then --check for hazards
 
-PC_out <= PC - 8;
+	elsif rising_edge(clk) then --pass new instruction
+		IR <= unsigned(IR_data);
+		PC_out <= PC - 8;
+	end if;
+end process;
+
+IR_track : process(clk)
+begin
+	if rising_edge(clk)
+		IR_log(4) <= IR_log(3);
+		IR_log(3) <= IR_log(2);
+		IR_log(2) <= IR_log(1);
+		IR_log(1) <= IR_check;
+	end if;
+end process;
 
 PC_update : process(IR_busy, clk)
 begin
